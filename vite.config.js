a6c3102f020ffa6fb86ff643cfca071018b07e59
Yur3x <juraj.durmek7@gmail.com>
2025-12-28 00:03:34 +0100

    pokus o favicony a pwa

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,42 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
     base: './',
-    plugins: [vue(), vueDevTools()],
+    plugins: [
+        vue(),
+        vueDevTools(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: [
+                'favicon.ico',
+                'apple-touch-icon.png',
+                'pwa-192x192.png',
+                'pwa-512x512.png',
+            ],
+            manifest: {
+                name: 'TanksGame',
+                short_name: 'TanksGame',
+                description: 'Awesome artillery tanks game built with Vue',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
